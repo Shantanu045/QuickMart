2fdf731302d640f9d93c3094631ea47787652e38
Shantanu045 <gawandeshantanu6@gmail.com>
2025-07-16 23:03:44 +0530

    Initial commit

---
+++ Render.yaml/ui/js/custom/common.js
@@ -0,0 +1,82 @@
+// Define your api here
+var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
+var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
+var productSaveApiUrl = 'https://quickmart-api.onrender.com/insertProduct';
+var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
+var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
+var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
+
+// For product drop in order
+var productsApiUrl = 'https://fakestoreapi.com/products';
+
+// function callApi(method, url, data) {
+//     $.ajax({
+//         method: method,
+//         url: url,
+//         data: data
+//     }).done(function( msg ) {
+//         window.location.reload();
+//     });
+// }
+
+
+function callApi(method, url, data) {
+    $.ajax({
+        method: method,
+        url: url,
+        data: JSON.stringify(data),
+        contentType: "application/json"
+    }).done(function(response) {
+        alert("Product inserted!");
+        window.location.reload();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        alert("Error: " + errorThrown);
+        console.error(jqXHR.responseText);
+    });
+}
+
+
+
+
+function calculateValue() {
+    var total = 0;
+    $(".product-item").each(function( index ) {
+        var qty = parseFloat($(this).find('.product-qty').val());
+        var price = parseFloat($(this).find('#product_price').val());
+        price = price*qty;
+        $(this).find('#item_total').val(price.toFixed(2));
+        total += price;
+    });
+    $("#product_grand_total").val(total.toFixed(2));
+}
+
+function orderParser(order) {
+    return {
+        id : order.id,
+        date : order.employee_name,
+        orderNo : order.employee_name,
+        customerName : order.employee_name,
+        cost : parseInt(order.employee_salary)
+    }
+}
+
+function productParser(product) {
+    return {
+        id : product.id,
+        name : product.employee_name,
+        unit : product.employee_name,
+        price : product.employee_name
+    }
+}
+
+function productDropParser(product) {
+    return {
+        id : product.id,
+        name : product.title
+    }
+}
+
+//To enable bootstrap tooltip globally
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// });
